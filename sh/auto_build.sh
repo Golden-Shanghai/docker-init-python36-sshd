12a1c70bd4861683730977ccb92566ae525a6845
zed.zhao <954489346zyz@sina.com>
2022-01-04 18:26:49 +0800

    fix

--- sh/auto_build.sh
+++ sh/auto_build.sh
@@ -4,7 +4,7 @@ CURDIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd )
 
 cd ${CURDIR}"/../"
 
-docker build -t python:36gd
+docker build -t python:36gd .
 
 docker-compose up -d
 
--- sh/init.sh
+++ sh/init.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-requirements_path="/docker/requirements.txt"
+requirements_path="/home/wwwroot/cx_bigdata_dts/requirements.txt"
 
 cmd="pip install --upgrade pip"
 
 # 判断是否存在依赖管理文件
 if [ -d ${requirements_path} ];then
-
     # 存在，则通过管理文件跑依赖
     $cmd=${$cmd}"&& pip install -i https://mirrors.aliyun.com/pypi/simple/ -r "${requirements_path}
 fi
